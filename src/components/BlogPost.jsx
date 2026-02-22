ac062cf0007cb58be3249fa0063fe5372da593e3
Azmattullah <azmattullah0@gmail.com>
2026-02-22 22:28:29 +0530

    Remove mock comments section from blog posts

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -42,48 +42,6 @@ const BlogPost = ({ post, onBack }) => {
                     <a href="#" className="author-link"><span className="link-icon">X</span> Twitter</a>
                     <a href="#" className="author-link"><span className="link-icon">in</span> LinkedIn</a>
                 </div>
-
-                <div className="comments-header">
-                    <h3>0 Comments</h3>
-                </div>
-
-                <div className="reaction-box">
-                    <h4 className="reaction-prompt">What do you think?</h4>
-                    <p className="reaction-subtitle">0 Responses</p>
-
-                    <div className="reaction-emojis">
-                        <button className="reaction-btn">👍<span className="reaction-label">Upvote</span></button>
-                        <button className="reaction-btn">😂<span className="reaction-label">Funny</span></button>
-                        <button className="reaction-btn">😍<span className="reaction-label">Love</span></button>
-                        <button className="reaction-btn">😮<span className="reaction-label">Surprised</span></button>
-                        <button className="reaction-btn">😡<span className="reaction-label">Angry</span></button>
-                        <button className="reaction-btn">😢<span className="reaction-label">Sad</span></button>
-                    </div>
-                </div>
-
-                {/* Mock Comment Input Area */}
-                <div className="mock-comment-area">
-                    <div className="comment-stats">
-                        <span>0 Komentar</span>
-                        <span className="login-link">Masuk ▼</span>
-                    </div>
-
-                    <div className="comment-input-box">
-                        <div className="avatar-placeholder">G</div>
-                        <div className="input-field">Mulai berdiskusi...</div>
-                    </div>
-
-                    <div className="comment-footer">
-                        <span>MASUK DENGAN</span>
-                        <div className="mock-social-icons">
-                            <span className="mock-icon blue">D</span>
-                            <span className="mock-icon light-blue">f</span>
-                            <span className="mock-icon black">X</span>
-                            <span className="mock-icon red">G</span>
-                        </div>
-                        <span className="or-text">ATAU DAFTAR DI DISQUS</span>
-                    </div>
-                </div>
             </section>
         </article>
     );
